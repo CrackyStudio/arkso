beb9b8d251c917a4e74718b45a719db37cc05e38
Antoine Nivoy <antoine.nivoy@gmail.com>
2018-09-25 20:36:05 +0200

    commit

---
+++ 4.1/ARKSO/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("ARK Server Organiser")>
+<Assembly: AssemblyDescription("ARK Server Organiser")>
+<Assembly: AssemblyCompany("Cracky Studio")>
+<Assembly: AssemblyProduct("ARK Server Organiser")>
+<Assembly: AssemblyCopyright("GNU General Public License v3.0")>
+<Assembly: AssemblyTrademark("Cracky Studio")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("e3df7d7d-f53e-4540-9cf5-da514b7655d2")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("4.1")>
+<Assembly: AssemblyFileVersion("4.1")>
