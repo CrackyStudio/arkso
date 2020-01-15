b1a5cbd6ddb047f5535fe924857d6421c2f2b934
Antoine <antoine.nivoy@gmail.com>
2020-01-15 22:19:35 +0100

    Update main graphics

--- Graphics/Clear.cs
+++ Graphics/Clear.cs
@@ -30,6 +30,16 @@ namespace ARKSO.Graphics
         public static void Main(Grid Grid)
         {
             Grid.Children.Remove(Graphics.Main.versionLabel);
+            Grid.Children.Remove(Graphics.Main.donateLabel);
+            Grid.Children.Remove(Graphics.Main.serverBox);
+            Grid.Children.Remove(Graphics.Main.statusDescLabel);
+            Grid.Children.Remove(Graphics.Main.statusLabel);
+            Grid.Children.Remove(Graphics.Main.latencyDescLabel);
+            Grid.Children.Remove(Graphics.Main.latencyLabel);
+            Grid.Children.Remove(Graphics.Main.addressDescLabel);
+            Grid.Children.Remove(Graphics.Main.addressLabel);
+            Grid.Children.Remove(Graphics.Main.shareAddressButton);
+            Grid.Children.Remove(Graphics.Main.settingsBox);
             Grid.Children.Remove(Graphics.Main.nameLabel);
             Grid.Children.Remove(Graphics.Main.nameTextbox);
             Grid.Children.Remove(Graphics.Main.mapLabel);
@@ -44,14 +54,25 @@ namespace ARKSO.Graphics
             Grid.Children.Remove(Graphics.Main.passwordTextbox);
             Grid.Children.Remove(Graphics.Main.adminPasswordLabel);
             Grid.Children.Remove(Graphics.Main.adminPasswordTextbox);
+            Grid.Children.Remove(Graphics.Main.portLabel);
+            Grid.Children.Remove(Graphics.Main.portTextbox);
+            Grid.Children.Remove(Graphics.Main.queryPortLabel);
+            Grid.Children.Remove(Graphics.Main.queryPortTextbox);
             Grid.Children.Remove(Graphics.Main.argumentsLabel);
             Grid.Children.Remove(Graphics.Main.argumentsTextbox);
             Grid.Children.Remove(Graphics.Main.optionsLabel);
             Grid.Children.Remove(Graphics.Main.optionsTextbox);
+            Grid.Children.Remove(Graphics.Main.manualConfigLabel);
+            Grid.Children.Remove(Graphics.Main.gameIniButton);
+            Grid.Children.Remove(Graphics.Main.gameUserSettingsIniButton);
+            Grid.Children.Remove(Graphics.Main.managementBox);
+            Grid.Children.Remove(Graphics.Main.hideConsoleCheckbox);
+            Grid.Children.Remove(Graphics.Main.actionBox);
             Grid.Children.Remove(Graphics.Main.turnOnOffButton);
             Grid.Children.Remove(Graphics.Main.updateButton);
-            Grid.Children.Remove(Graphics.Main.statusLabel);
-            Grid.Children.Remove(Graphics.Main.latencyLabel);
+            Grid.Children.Remove(Graphics.Main.backupButton);
+            Grid.Children.Remove(Graphics.Main.credits);
+
             Application.Current.MainWindow.Width = 800;
             Grid.Width = 800;
             MainWindow.CenterApp();          
